0807ca91e315a8f3760b90e006d4f17608a47a2b
Matheus A. Souza <ticksmas@gmail.com>
2016-09-08 22:04:37 -0300

    Tests on wrapper... Still bugged

--- papiCacheWrapper.c
+++ papiCacheWrapper.c
@@ -8,54 +8,69 @@
 #include <papi.h>
 #include <stdlib.h>
 
-#define NUM_EVENTS 4
-
 int main(int argc, char **argv)
 {
-    int event[NUM_EVENTS] = {PAPI_L2_TCA, PAPI_L2_TCM, PAPI_L3_TCA, PAPI_L3_TCM};
-    long long values[NUM_EVENTS];
+    int eventSet;
+    eventSet = PAPI_NULL;
     pid_t pid;
     char* newargv[10]; // FIXME: Improve args wrapper
-    int pidStatus, retval;
-    
-    /* Start counting events */
-    if (PAPI_start_counters(event, NUM_EVENTS) != PAPI_OK) {
-        fprintf(stderr, "PAPI_start_counters - FAILED\n");
-        exit(1);
+    int pidStatus, retval, numEvents, numEventSets;
+/*   int event[NUM_EVENTS] = {PAPI_L2_TCA, PAPI_L2_TCM, PAPI_L3_TCA, PAPI_L3_TCM}; */
+    long long values[4];
+
+
+    retval = PAPI_library_init(PAPI_VER_CURRENT);
+    if (retval != PAPI_VER_CURRENT) {
+	fprintf(stderr,"PAPI_library_init - FAILED\n", retval);
     }
 
-   /* Wrap your file */
+    /* Create EventSet */
+    if(PAPI_create_eventset(&eventSet) != PAPI_OK) {
+        fprintf(stderr, "PAPI_create_eventset - FAILED\n");
+    }
+
+    if((retval=PAPI_add_event(eventSet,PAPI_L2_TCA))!= PAPI_OK) fprintf(stderr, "PAPI_add_event - FAILED\n\t%s\n", retval);
+    if((retval=PAPI_add_event(eventSet,PAPI_L2_TCM))!= PAPI_OK) fprintf(stderr, "PAPI_add_event - FAILED\n\t%s\n", retval);
+    if((retval=PAPI_add_event(eventSet,PAPI_L3_TCA))!= PAPI_OK) fprintf(stderr, "PAPI_add_event - FAILED\n\t%s\n", retval);
+    if((retval=PAPI_add_event(eventSet,PAPI_L3_TCM))!= PAPI_OK) fprintf(stderr, "PAPI_add_event - FAILED\n\t%s\n", retval);
+
+    /* Wrap your file */
     if(!(pid = fork())) {
         /* Need to set inherit options */
-        if ( (retval = PAPI_attach(*event, pid)) != PAPI_OK) {
-            fprintf(stderr, "PAPI_attach - FAILED\n%s\n", PAPI_strerror(retval));
+        if( (retval = PAPI_attach(eventSet, pid)) != PAPI_OK) {
+            fprintf(stderr, "PAPI_attach - FAILED\n\t%s\n", PAPI_strerror(retval));
             exit(1);
         }
+
+	/* Start counting events */
+	if ( (retval = PAPI_start(eventSet)) != PAPI_OK) {
+            fprintf(stderr, "PAPI_start - FAILED\n\t%s\n", retval);
+            exit(1);
+    	}
+
         execv(argv[1], argv);
+
+	if( (retval = PAPI_detach(eventSet)) != PAPI_OK) {
+	    fprintf(stderr, "PAPI_detach - FAILED\n\t%s\n", PAPI_strerror(retval));
+            exit(1);
+	}
+
+	/* Read the counters and stop */
+	if ( (retval = PAPI_stop(eventSet,values)) != PAPI_OK) {
+            fprintf(stderr, "PAPI_stop - FAILED\n\t%s\n", retval);
+            exit(1);
+	}
+
     }
     waitpid(pid, pidStatus, 0);
 
-    /* Read the counters */
-    if (PAPI_read_counters(values, NUM_EVENTS) != PAPI_OK) {
-        fprintf(stderr, "PAPI_read_counters - FAILED\n");
-        exit(1);
-    }
-
     /* Show readings */
-    /* FIXME: The case when L3 does not exist... */
     printf("# of L2 cache access	= %lld\n", values[0]);
     printf("# of L2 cache misses	= %lld\n", values[1]);
-    printf("	%% of L2 cache misses	= %2.3f\n\n",(double) values[1] / (double) values[0]);
+//  printf("	%% of L2 cache misses	= %2.3f\n\n",(double) *values[1] / (double) *values[0]);
     printf("# of L3 cache access	= %lld\n", values[2]);
     printf("# of L3 cache misses	= %lld\n", values[3]);
-    printf("	%% of L3 cache misses	= %2.3f\n\n", (double) values[3] / (double) values[2]);
-
+//    printf("	%% of L3 cache misses	= %2.3f\n\n", (double) *values[3] / (double) *values[2]);
 	
-    /* Stop counting events */
-    if (PAPI_stop_counters(values, NUM_EVENTS) != PAPI_OK) {
-        fprintf(stderr, "PAPI_stoped_counters - FAILED\n");
-        exit(1);
-    }
-
     return 0;
 }
